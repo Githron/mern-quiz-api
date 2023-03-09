200033a60160d9a10911943030f1981165fcbd33
githron <allewnor@gmail.com>
2023-03-09 18:38:33 +0000

    init

--- models/Users.js
+++ models/Users.js
@@ -7,6 +7,10 @@ const UserSchema = new mongoose.Schema({
     score: {
         type: String,
     },
+
+    totalquiz: {
+        type: String,
+    }
 });
 
 const UserModel = mongoose.model("users", UserSchema);
